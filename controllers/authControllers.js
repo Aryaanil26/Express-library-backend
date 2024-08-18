81edb65756ac722e11ecda9185cd127f6d4fc193
arya <aryaanil9100@gmial.com>
2024-08-18 13:49:24 -0400

    updated  backend

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -13,8 +13,8 @@ if(!user){
 const passwordsMatch = bcrypt.compareSync(password, user.password);
 
 if(passwordsMatch) {
-    const token = token = jwt.sign({ _id: user._id,email: user.email, name:user.name },process.env.TOKEN_SECRET_KEY);
-    res.cookie('token',token, {httpOnly:true,secure: process.env.ENVIRONMMENT === "development" ? false :true, maxage:1 * 60 * 60 * 1000})
+    const token = jwt.sign({ _id: user._id,email: user.email, name:user.name }, process.env.TOKEN_SECRET_KEY);
+    res.cookie('token',token, { httpOnly:true, secure: process.env.ENVIRONMMENT === "development" ? false :true, maxage:1 * 60 * 60 * 1000})
     res.send(token)   
     
     res.json({_id:user._id, name: user.name, email: user.email })
